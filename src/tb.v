c92ff3f6cf7ec3120b2a43d91fe9a6ef4c8a0236
Erik Mercado <mercadoeirk1031@gmail.com>
2023-10-30 14:40:08 -0700

    update tb.v

--- src/tb.v
+++ src/tb.v
@@ -21,6 +21,32 @@ module tb ();
         rst_n = 1;
     end
 
+    // Initialization of Signals
+    initial begin
+        ena = 0;
+        matrixA_in = 8'b00000000; // Initial value: all zeros
+        matrixB_in = 8'b00000000; // Initial value: all zeros
+    end
+
+    // Test Vectors for the design
+    initial begin
+        #15;
+        ena = 1;
+        matrixA_in = 8'b01101110; // Example value: 1, 1, -1, 2
+        matrixB_in = 8'b11010101; // Example value: -1, 1, -1, 1
+        #10;
+        matrixA_in = 8'b10011001; // Another example value: 2, -1, -1, -1
+        matrixB_in = 8'b01100110; // Another example value: 1, 0, 1, 2
+        #10;
+        ena = 0;
+        #10;
+        ena = 1;
+        matrixA_in = 8'b10110101; // Yet another example value: 2, 1, -1, 1
+        matrixB_in = 8'b01101101; // Yet another example value: 1, 1, 1, -1
+        #100;
+        $finish;  // End the simulation after some time
+    end
+
     // wire up the inputs and outputs
     reg  clk;
     reg  rst_n;
@@ -41,6 +67,6 @@ module tb ();
         .ena        (ena),            // enable - goes high when design is selected
         .clk        (clk),            // clock
         .rst_n      (rst_n)           // not reset
-        );
+    );
 
 endmodule
