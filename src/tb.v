8ed00400798543438c14913c2fe8fdc613387099
Erik Mercado <mercadoeirk1031@gmail.com>
2023-10-30 16:01:20 -0700

    update tb.v

--- src/tb.v
+++ src/tb.v
@@ -30,19 +30,22 @@ module tb ();
 
     // Test Vectors for the design
     initial begin
-        #15;
-        ena = 1;
-        matrixA_in = 8'b01101110; // Example value: 1, 1, -1, 2
-        matrixB_in = 8'b11010101; // Example value: -1, 1, -1, 1
-        #10;
-        matrixA_in = 8'b10011001; // Another example value: 2, -1, -1, -1
-        matrixB_in = 8'b01100110; // Another example value: 1, 0, 1, 2
-        #10;
-        ena = 0;
-        #10;
-        ena = 1;
-        matrixA_in = 8'b10110101; // Yet another example value: 2, 1, -1, 1
-        matrixB_in = 8'b01101101; // Yet another example value: 1, 1, 1, -1
+        // #15;
+        // ena = 1;
+        // matrixA_in = 8'b01101110; // Example value: 1, 1, -1, 2
+        // matrixB_in = 8'b11010101; // Example value: -1, 1, -1, 1
+        // #10;
+        // matrixA_in = 8'b10011001; // Another example value: 2, -1, -1, -1
+        // matrixB_in = 8'b01100110; // Another example value: 1, 0, 1, 2
+        // #10;
+        // ena = 0;
+        // #10;
+        // ena = 1;
+        // matrixA_in = 8'b10110101; // Yet another example value: 2, 1, -1, 1
+        // matrixB_in = 8'b01101101; // Yet another example value: 1, 1, 1, -1
+        #10
+        matrixA_in = 0b00100010
+        matrixB_in = 0b00100010
         #100;
         $finish;  // End the simulation after some time
     end
